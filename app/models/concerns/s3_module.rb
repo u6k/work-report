6da9e76cc7292cd796318be751ff1ad0bd1453e6
u6k <u6k.apps@gmail.com>
2018-05-25 21:53:38 +0900

    WIP: impl redmine activity model method, and test.

---
+++ app/models/concerns/s3_module.rb
@@ -0,0 +1,8 @@
+module S3Module
+  extend ActiveSupport::Concern
+
+  def self.get(s3_object_key)
+    raise "TODO" # TODO
+  end
+
+end
--- test/models/redmine_activity_test.rb
+++ test/models/redmine_activity_test.rb
@@ -50,4 +50,74 @@ class RedmineActivityTest < ActiveSupport::TestCase
     assert_not activity.valid?
   end
 
+  test "download redmine activity atom" do
+    # precondition
+    assert_not S3Module.get("redmine_activity.latest.atom").exists?
+    assert_equal 0, RedmineActivity.all.length
+
+    # execute
+    s3_object_keys = RedmineActivity.download_redmine_activity_atom("https://redmine.u6k.me/activity.atom?key=6c9b0fe50d9bdcc2e4da1215b7187aededf36652&user_id=5")
+
+    activities = RedmineActivity.parse_redmine_activity_atom(s3_object_keys[:original])
+
+    # postcondition
+    assert_equal "redmine_activity.latest.atom", s3_object_keys[:original]
+    assert_match /^redmine_activity\.\d{8}_\d{6}\.atom$/, s3_object_keys[:backup]
+
+    assert S3Module.get(s3_object_keys[:original]).exists?
+    assert S3Module.get(s3_object_keys[:backup]).exists?
+
+    assert activities.length > 0
+    assert_equal 0, RedmineActivity.all.length
+  end
+
+  test "import redmine activity atom" do
+    # setup
+    S3Module.get("redmine_activity.latest.atom").put(File.read("test/fixtures/files/redmine_activity.atom"))
+
+    # precondition
+    assert_equal 0, RedmineActivity.all.length
+
+    # execute
+    activities = RedmineActivity.parse_redmine_activity_atom("redmine_activity.latest.atom")
+    activity_ids = RedmineActivity.import(activities)
+
+    # postcondition
+    assert_equal 0, activities.length
+    assert_equal activities.length, activity_ids.length
+
+    activities.each do |activity|
+      activity_actual = RedmineActivity.find_by(entry_id: activity.entry_id)
+      assert_same activity, activity_actual
+    end
+  end
+
+  test "import duplicate redmine activity" do
+    # precondition
+    assert_equal 0, RedmineActivity.all.length
+
+    # execute
+    activities = [
+      RedmineActivity.new(entry_title: "aaa", entry_link: "http://example.redmine.com/issue/aaa", entry_id: "aaa", entry_updated: DateTime.current),
+      RedmineActivity.new(entry_title: "bbb", entry_link: "http://example.redmine.com/issue/bbb", entry_id: "bbb", entry_updated: DateTime.current),
+      RedmineActivity.new(entry_title: "ccc", entry_link: "http://example.redmine.com/issue/ccc", entry_id: "ccc", entry_updated: DateTime.current)
+    ]
+    activity_ids  = RedmineActivity.import(activities)
+
+    assert_equal 3, activity_ids.length
+    assert_equal 3, RedmineActivity.all.length
+
+    activities << RedmineActivity.new(entry_title: "ddd", entry_link: "http://example.redmine.com/issue/ddd", entry_id: "ddd", entry_updated: DateTime.current)
+    activity_ids = RedmineActivity.import(activities)
+
+    # postcondition
+    assert_equal 1, activity_ids
+    assert_equal 4, RedmineActivity.all.length
+
+    activities.each do |activity|
+      activity_actual = RedmineActivity.find_by(entry_id: activity.entry_id)
+      assert_same activity, activity_actual
+    end
+  end
+
 end
