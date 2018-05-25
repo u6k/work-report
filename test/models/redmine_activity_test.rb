0a143cb0725a19eef823665b72b5bce8a4fac8bf
u6k <u6k.apps@gmail.com>
2018-05-25 19:07:20 +0900

    WIP

--- test/models/redmine_activity_test.rb
+++ test/models/redmine_activity_test.rb
@@ -29,11 +29,20 @@ class RedmineActivityTest < ActiveSupport::TestCase
   end
 
   test "invalid model: id empty" do
-    flunk
+    activity = RedmineActivity.new(entry_title: "updated - Redmine", entry_link: "http://example.redmine.com/issue/1#change=12345?page=1", entry_updated: DateTime.strptime("2018-05-25T01:23:45Z"))
+    assert_not activity.valid?
+
+    activity = RedmineActivity.new(entry_title: "updated - Redmine", entry_link: "http://example.redmine.com/issue/1#change=12345?page=1", entry_id: " ", entry_updated: DateTime.strptime("2018-05-25T01:23:45Z"))
+    assert_not activity.valid?
   end
 
   test "invalid model: id not unique" do
-    flunk
+    RedmineActivity.new(entry_title: "updated 1 - Redmine", entry_link: "http://example.redmine.com/issue/1", entry_id: "1", entry_updated: DateTime.strptime("2018-05-25T00:00:00Z")).save!
+    RedmineActivity.new(entry_title: "updated 2 - Redmine", entry_link: "http://example.redmine.com/issue/2", entry_id: "2", entry_updated: DateTime.strptime("2018-05-25T00:00:00Z")).save!
+    RedmineActivity.new(entry_title: "updated 3 - Redmine", entry_link: "http://example.redmine.com/issue/3", entry_id: "3", entry_updated: DateTime.strptime("2018-05-25T00:00:00Z")).save!
+
+    activity = RedmineActivity.new(entry_title: "updated 2 - Redmine", entry_link: "http://example.redmine.com/issue/2", entry_id: "2", entry_updated: DateTime.strptime("2018-05-25T00:00:00Z"))
+    assert_not activity.valid?
   end
 
   test "invalid model: updated empty" do
