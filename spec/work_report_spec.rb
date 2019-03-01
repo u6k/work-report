9b00608f030a5024ede9d291a8c9bbd465ffd1d2
u6k <u6k.apps@gmail.com>
2019-03-01 21:33:33 +0900

    Changed: remove unnecessary test

--- spec/work_report_spec.rb
+++ spec/work_report_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe WorkReport do
   it "has a version number" do
     expect(WorkReport::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
