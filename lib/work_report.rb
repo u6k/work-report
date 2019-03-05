f4245e0041b9878f96fe50fd8286966e0a4cfdf4
u6k <u6k.apps@gmail.com>
2019-03-05 16:21:39 +0900

    Changed: impl github activity

--- lib/work_report.rb
+++ lib/work_report.rb
@@ -1,6 +1,81 @@
+require "json"
+require "csv"
+require "open-uri"
+
 require "work_report/version"
 
 module WorkReport
-  class Error < StandardError; end
-  # Your code goes here...
+  class GithubActivity
+    def initialize(user, github_token)
+      @user = user
+      @github_token = github_token
+    end
+
+    def commits_to_hash
+      # get contributions
+      url = "https://github-contributions-api.now.sh/v1/#{@user}"
+      
+      json = open(url) do |f|
+        JSON.load(f.read)
+      end
+      
+      # convert summary
+      counts = json["contributions"].map do |contribution|
+        {
+          "date" => Time.parse(contribution["date"]),
+          "count" => contribution["count"]
+        }
+      end
+    end
+
+    def releases_to_hash
+      # get repos
+      repos_url = "https://api.github.com/users/#{@user}/repos"
+      
+      repos = get_github_data(repos_url)
+      
+      # get releases
+      releases = repos.flat_map do |repo|
+        releases_url = "https://api.github.com/repos/#{@user}/#{repo["name"]}/releases"
+      
+        releases = get_github_data(releases_url)
+      end
+      
+      releases.compact!
+      
+      # convert release summary
+      release_summaries = releases.map do |release|
+        {
+          "url" => release["html_url"],
+          "name" => release["name"],
+          "created_at" => Time.parse(release["created_at"])
+        }
+      end
+    end
+
+    private
+
+    def get_github_data(url)
+      data = open(url, "Authorization" => "token #{@github_token}") do |f|
+        sleep 1
+    
+        current_data = JSON.load(f.read)
+    
+        if not f.meta["link"].nil?
+          parent_data = f.meta["link"].match(/<([^<>]+)>; rel=\"next\"/) do |link|
+            get_github_data(link[1])
+          end
+        end
+    
+        if parent_data.nil?
+          current_data
+        else
+          current_data + parent_data
+        end
+      end
+    
+      data
+    end
+  end
 end
+
--- spec/work_report_spec.rb
+++ spec/work_report_spec.rb
@@ -2,4 +2,38 @@ RSpec.describe WorkReport do
   it "has a version number" do
     expect(WorkReport::VERSION).not_to be nil
   end
+
+  describe WorkReport::GithubActivity do
+    before do
+      @github_activity = WorkReport::GithubActivity.new("u6k", ENV["GITHUB_TOKEN"])
+    end
+
+    describe "#commits_to_hash" do
+      it "is hash" do
+        commits = @github_activity.commits_to_hash
+
+        expect(commits.size).to be > 0
+
+        commits.each do |commit|
+          expect(commit["date"]).to be_is_a Time
+          expect(commit["count"]).to be_integer
+        end
+      end
+    end
+
+    describe "#releases_to_hash" do
+      it "is hash" do
+        releases = @github_activity.releases_to_hash
+
+        expect(releases.size).to be > 0
+
+        releases.each do |release|
+          expect(release["url"]).to start_with "https://github.com/u6k/"
+          expect(release["name"]).to match /\w+/
+          expect(release["created_at"]).to be_is_a Time
+        end
+      end
+    end
+  end
 end
+
